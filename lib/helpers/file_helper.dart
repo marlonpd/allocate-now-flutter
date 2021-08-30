851e00f132b25aa54c6618dac0ebd5d4c3df2f5a
marlonpd <send8heremd@gmail.com>
2021-08-30 18:41:20 +0800

    Fix generating backup filename

--- lib/helpers/file_helper.dart
+++ lib/helpers/file_helper.dart
@@ -6,9 +6,33 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileHelper {
+  String appFoldername = 'back';
+
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
 
     return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<File> writeBackUp(String content) async {
+    final file = await _generateFilename;
+
+    // Write the file
+    return file.writeAsString('$content');
+  }
+
+  Future<File> get _generateFilename async {
+    Directory appDocumentsDirectory =
+        await getApplicationDocumentsDirectory(); // 1
+    int filename = DateTime.now().millisecondsSinceEpoch;
+    String appDocumentsPath = appDocumentsDirectory.path; // 2
+    String filePath = '$appDocumentsPath/$appFoldername/$filename.dat'; // 3
+
+    return File(filePath);
   }
 }
--- lib/helpers/google_drive.dart
+++ lib/helpers/google_drive.dart
@@ -34,124 +34,110 @@ class DriveHelper {
   }
 
   // Upload a file to Google Drive.
-  Future uploadFile(BuildContext buildContext) async {
-    if (currentUser == null) {
-      launchPage(buildContext, WelcomePage());
-      return;
-    }
+  // Future uploadFile(BuildContext buildContext) async {
+  //   if (currentUser == null) {
+  //     launchPage(buildContext, WelcomePage());
+  //     return;
+  //   }
 
-    io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, DatabaseHelper.dataBaseName);
+  //   io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+  //   String path = join(documentsDirectory.path, DatabaseHelper.dataBaseName);
 
-    //
-    GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
-    var api = new drive.DriveApi(client);
+  //   //
+  //   GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
+  //   var api = new drive.DriveApi(client);
 
-    var localFile = new io.File(path);
-    var media = new drive.Media(localFile.openRead(), localFile.lengthSync());
-    drive.File driveFile = new drive.File()
-      ..title = DatabaseHelper.dataBaseName;
-    return (await api.files.insert(
-      driveFile,
-      uploadMedia: media,
-    ))
-        .id;
-  }
+  //   var localFile = new io.File(path);
+  //   var media = new drive.Media(localFile.openRead(), localFile.lengthSync());
+  //   drive.File driveFile = new drive.File()
+  //     ..title = DatabaseHelper.dataBaseName;
+  //   return (await api.files.insert(
+  //     driveFile,
+  //     uploadMedia: media,
+  //   ))
+  //       .id;
+  // }
 
   // Upload a file to Google Drive.
-  Future deleteFile(BuildContext buildContext, drive.File file) async {
-    if (currentUser == null) {
-      launchPage(buildContext, WelcomePage());
-      return;
-    }
+  // Future deleteFile(BuildContext buildContext, drive.File file) async {
+  //   if (currentUser == null) {
+  //     launchPage(buildContext, WelcomePage());
+  //     return;
+  //   }
 
-    //
-    GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
-    var api = new drive.DriveApi(client);
+  //   //
+  //   GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
+  //   var api = new drive.DriveApi(client);
 
-    await api.files.delete(file.id);
-  }
+  //   await api.files.delete(file.id);
+  // }
 
 // Download a file from Google Drive.
-  Future downloadFile(BuildContext buildContext, drive.File file) async {
-    if (currentUser == null) {
-      launchPage(buildContext, WelcomePage());
-      return false;
-    }
+  // Future downloadFile(BuildContext buildContext, drive.File file) async {
+  //   if (currentUser == null) {
+  //     launchPage(buildContext, WelcomePage());
+  //     return false;
+  //   }
 
-    io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, DatabaseHelper.dataBaseName);
+  //   io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+  //   String path = join(documentsDirectory.path, DatabaseHelper.dataBaseName);
 
-    performDriveRequest(
-      buildContext: buildContext,
-      file: io.File(path),
-      headers: await currentUser.authHeaders,
-      url: file.downloadUrl ?? '',
-    );
+  //   performDriveRequest(
+  //     buildContext: buildContext,
+  //     file: io.File(path),
+  //     headers: await currentUser.authHeaders,
+  //     url: file.downloadUrl ?? '',
+  //   );
 
-    return true;
-    //
-  }
+  //   return true;
+  //   //
+  // }
 
-  Future<List<File>> getAllFiles(BuildContext buildContext) async {
-    if (currentUser == null) {
-      launchPage(buildContext, WelcomePage());
-      return [];
-    }
-    GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
-    var api = new drive.DriveApi(client);
+  // Future<List<File>> getAllFiles(BuildContext buildContext) async {
+  //   if (currentUser == null) {
+  //     launchPage(buildContext, WelcomePage());
+  //     return [];
+  //   }
+  //   GoogleHttpClient client = GoogleHttpClient(await currentUser.authHeaders);
+  //   var api = new drive.DriveApi(client);
 
-    return (await api.files.list()).items;
-  }
+  //   return (await api.files.list()).items;
+  // }
 
-  Future performDriveRequest(
-      {@required BuildContext buildContext,
-      @required io.File file,
-      @required Map<String, String> headers,
-      @required String url}) async {
-    HttpClient httpClient = new HttpClient();
+  // Future performDriveRequest(
+  //     {@required BuildContext buildContext,
+  //     @required io.File file,
+  //     @required Map<String, String> headers,
+  //     @required String url}) async {
+  //   HttpClient httpClient = new HttpClient();
 
-    HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
+  //   HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
 
-    headers.forEach((k, v) {
-      request.headers.set(k, v);
-    });
+  //   headers.forEach((k, v) {
+  //     request.headers.set(k, v);
+  //   });
 
-    HttpClientResponse response = await request.close();
+  //   HttpClientResponse response = await request.close();
 
-    if (response.statusCode == 200) {
-      var _downloadData = List<int>();
-      response.listen((d) => _downloadData.addAll(d), onDone: () {
-        file.writeAsBytes(_downloadData).then((v) {
-          showDistivityDialog(buildContext,
-              actions: [
-                getButton('Refresh', onPressed: () {
-                  MyApp.dataModel = null;
-                  DistivityRestartWidget.restartApp(buildContext);
-                })
-              ],
-              title: 'Calendars and events downloaded', stateGetter: (ctx, ss) {
-            return getText(
-                'Your calendars and your events from your last saved file from drive are now on your device');
-          });
-        });
-      });
-    } else {
-      return null;
-    }
-  }
-}
-
-class GoogleHttpClient extends IOClient {
-  Map<String, String> _headers;
-
-  GoogleHttpClient(this._headers) : super();
-
-  @override
-  Future<IOStreamedResponse> send(BaseRequest request) =>
-      super.send(request..headers.addAll(_headers));
-
-  @override
-  Future<Response> head(Object url, {Map<String, String> headers}) =>
-      super.head(url, headers: headers..addAll(_headers));
+  //   if (response.statusCode == 200) {
+  //     var _downloadData = List<int>();
+  //     response.listen((d) => _downloadData.addAll(d), onDone: () {
+  //       file.writeAsBytes(_downloadData).then((v) {
+  //         showDistivityDialog(buildContext,
+  //             actions: [
+  //               getButton('Refresh', onPressed: () {
+  //                 MyApp.dataModel = null;
+  //                 DistivityRestartWidget.restartApp(buildContext);
+  //               })
+  //             ],
+  //             title: 'Calendars and events downloaded', stateGetter: (ctx, ss) {
+  //           return getText(
+  //               'Your calendars and your events from your last saved file from drive are now on your device');
+  //         });
+  //       });
+  //     });
+  //   } else {
+  //     return null;
+  //   }
+  // }
 }
