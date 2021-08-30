749dd3ed3a9b41dc58cd2be5a3f68c2c119dddad
marlonpd <send8heremd@gmail.com>
2021-08-30 17:49:01 +0800

    Add file and gdrive helpers

--- lib/helpers/file_helper.dart
+++ lib/helpers/file_helper.dart
@@ -1,1 +1,14 @@
-class FileHelper {}
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FileHelper {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+}
